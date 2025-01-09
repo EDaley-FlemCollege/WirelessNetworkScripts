65745d2bd5c52625812101ded0fc977958535c2f
EDaley-FlemCollege <122706767+EDaley-FlemCollege@users.noreply.github.com>
2025-01-09 09:55:21 -0500

    Pulled ScanScript.py from COMP630-Bonus
    This will act as a template for ScanScript

---
+++ ScanScript.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from threading import Thread
+import time,sys,subprocess,os
+from scapy.all import Dot11, Dot11Deauth, Dot11Disas, RadioTap, Dot11Elt, sendp, sniff, conf, EAPOL, Dot11EltRSN
+
+channels = [1,2,3,4,5,6,7,8,9,10,11,32,36,40,44,48,52,56,60,64,68,96,100,104,108,112,116,120,124,128,132,136,140,144,149,153,157,161,165,169,173,177]
+hmac = '5A:6D:67:AC:90:90'
+index = 0
+id_list = []
+
+s=conf.L2socket(iface='wlan0')
+
+subprocess.run("sudo airmon-ng check kill > /dev/null", shell=True, executable="/bin/bash")
+subprocess.run("sudo airmon-ng start wlan0 > /dev/null", shell=True, executable="/bin/bash")
+change_channel="sudo iwconfig wlan0 channel "+ str(channels[index])
+subprocess.run(change_channel, shell=True, executable="/bin/bash")
+
+def Process_Frame(packet):
+	global id_list
+	if packet.type == 0 and packet.subtype == 5:
+		ssid = str(packet[Dot11Elt].info)
+		ssid = ssid.split("'")
+		id_stat = [ssid[1],packet.addr2]
+		id_list.append(id_stat)
+
+def counter():
+	global channels
+	global index
+	global id_list
+	while True:
+		id_list = []
+		time.sleep(2)
+		print(f"Channel: {channels[index]}")
+		#id_list = list(dict.fromkeys(id_list))
+		print(id_list)
+		print("")
+		index +=1
+		if index > 41:
+			index = 0
+		change_channel="sudo iwconfig wlan0 channel "+ str(channels[index])
+		subprocess.run(change_channel, shell=True, executable="/bin/bash")
+		probe_req=RadioTap()/Dot11(type=0,subtype=4,addr1='FF:FF:FF:FF:FF:FF',addr2=hmac,addr3=hmac)/Dot11Elt(ID=0,info="",len=0)
+		for i in range(1,3):
+			s.send(probe_req)
+
+counter_thread = Thread(target=counter)
+counter_thread.daemon = True
+counter_thread.start()
+
+sniff(iface='wlan0',prn=Process_Frame,lfilter=lambda pkt: pkt.haslayer(Dot11), store=0)
